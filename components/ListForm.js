b477d902db591ac5a88e281074dcd495bef4e6e5
Hilary B <hilarybontz@gmail.com>
2021-01-31 16:08:33 -0800

    ui updates

--- components/ListForm.js
+++ components/ListForm.js
@@ -2,38 +2,44 @@ import EasyEdit, { Types } from "react-easy-edit";
 
 function ListForm(props) {
   return (
-    <div>
-      <EasyEdit
-        type={Types.TEXT}
-        onSave={props.onSaveName}
-        onCancel={props.onCancel}
-        saveButtonLabel="Save"
-        cancelButtonLabel="Cancel"
-        placeholder="Item Name"
-        cancelButtonStyle="ml-3 inline-flex justify-center py-.5 px-1 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-        saveButtonStyle="ml-3 inline-flex justify-center py-.5 px-1 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-      />
-      <EasyEdit
-        type={Types.TEXT}
-        onSave={props.onSaveUrl}
-        onCancel={props.onCancel}
-        saveButtonLabel="Save"
-        cancelButtonLabel="Cancel"
-        placeholder="Item Url"
-        cancelButtonStyle="ml-3 inline-flex justify-center py-.5 px-1 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-        saveButtonStyle="ml-3 inline-flex justify-center py-.5 px-1 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-      />
-      <EasyEdit
-        type={Types.TEXT}
-        onSave={props.onSaveNote}
-        onCancel={props.onCancel}
-        saveButtonLabel="Save"
-        cancelButtonLabel="Cancel"
-        placeholder="Item Note"
-        cancelButtonStyle="ml-3 inline-flex justify-center py-.5 px-1 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-        saveButtonStyle="ml-3 inline-flex justify-center py-.5 px-1 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-      />
-    </div>
+    <ul>
+      <li>
+        <EasyEdit
+          type={Types.TEXT}
+          onSave={props.onSaveName}
+          onCancel={props.onCancel}
+          saveButtonLabel="Save"
+          cancelButtonLabel="Cancel"
+          placeholder="Item Name"
+          cancelButtonStyle="ml-3 inline-flex justify-center py-.5 px-1 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          saveButtonStyle="ml-3 inline-flex justify-center py-.5 px-1 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        />
+      </li>
+      <li>
+        <EasyEdit
+          type={Types.TEXT}
+          onSave={props.onSaveUrl}
+          onCancel={props.onCancel}
+          saveButtonLabel="Save"
+          cancelButtonLabel="Cancel"
+          placeholder="Item Url"
+          cancelButtonStyle="ml-3 inline-flex justify-center py-.5 px-1 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          saveButtonStyle="ml-3 inline-flex justify-center py-.5 px-1 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        />
+      </li>
+      <li>
+        <EasyEdit
+          type={Types.TEXT}
+          onSave={props.onSaveNote}
+          onCancel={props.onCancel}
+          saveButtonLabel="Save"
+          cancelButtonLabel="Cancel"
+          placeholder="Item Note"
+          cancelButtonStyle="ml-3 inline-flex justify-center py-.5 px-1 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          saveButtonStyle="ml-3 inline-flex justify-center py-.5 px-1 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        />
+      </li>
+    </ul>
   );
 }
 
--- pages/list.js
+++ pages/list.js
@@ -1,6 +1,5 @@
 import { Component } from "react";
 import EasyEdit, { Types } from "react-easy-edit";
-//import ListItem from "../components/ListItem.js";
 import ListHeader from "../components/ListHeader.js";
 import ListForm from "../components/ListForm";
 
@@ -11,11 +10,14 @@ class List extends Component {
     name: "",
     url: "",
     note: "",
-    addItemClicked: false,
+    addItem: [0],
   };
 
   handleAddItem = () => {
-    this.setState({ addItemClicked: true });
+    var addItem = this.state.addItem;
+    addItem.push(0)
+    this.setState({ addItem: addItem });
+
   };
 
   saveMessage = (value) => {
@@ -59,46 +61,34 @@ class List extends Component {
         <button
           onClick={() => {
             this.handleAddItem();
-            console.log("add item to list");
           }}
           type="submit"
           class="ml-3 inline-flex justify-center py-.5 px-1 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
         >
           Add an Item
         </button>
-        <ListForm
-          onSaveName={(value) => {
-            this.saveName(value);
-          }}
-          onSaveUrl={(value) => {
-            this.saveUrl(value);
-          }}
-          onSaveNote={(value) => {
-            this.saveNote(value);
-          }}
-          onCancel={() => {
-            this.cancel();
-          }}
-        />
 
-        {this.state.addItemClicked ? (
-          <ListForm
-            onSaveName={(value) => {
-              this.saveName(value);
-            }}
-            onSaveUrl={(value) => {
-              this.saveUrl(value);
-            }}
-            onSaveNote={(value) => {
-              this.saveNote(value);
-            }}
-            onCancel={() => {
-              this.cancel();
-            }}
-          />
-        ) : (
-          <div></div>
-        )}
+
+        {this.state.addItem.map((x, i) => {
+                  return (
+                    <div key={i}>
+                  <ListForm
+                  onSaveName={(value) => {
+                    this.saveName(value);
+                  }}
+                  onSaveUrl={(value) => {
+                    this.saveUrl(value);
+                  }}
+                  onSaveNote={(value) => {
+                    this.saveNote(value);
+                  }}
+                  onCancel={() => {
+                    this.cancel();
+                  }}
+                />
+                </div>
+                  )
+        })}
       </div>
     );
   }
