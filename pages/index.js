a4206ba61d7b7ca01cd14b5216d8f82c7211738f
Hilary B <hilarybontz@gmail.com>
2021-02-20 11:41:46 -0800

    fetch data

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
         </h2>
         <div className="mt-8 flex justify-center">
           <div className="inline-flex rounded-md shadow">
-            <Link href="./list">
+            <Link href="/list">
             <a
               
               className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
